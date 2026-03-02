f49cb4a775ff600ab6da5aa042a36d09f3dc4864
thiepcuoionline-thanhchungmaianh <55303238+thiepcuoionline-thanhchungmaianh@users.noreply.github.com>
2026-03-02 15:02:02 +0700

    Add files via upload

---
+++ templates/template19/webfonts/simple-lightgallery/assets/select2/simple.lightgallery.select2.js
@@ -0,0 +1,7 @@
+jQuery( document ).ready( function ( $ ) {
+
+	$( '#lightgallery_post_types' ).select2( {width: "200px"} );
+	$( '#lightgallery_taxonomies' ).select2( {width: "200px"} );
+	$( '#lightgallery_selectors' ).select2( {width: "200px", tags: true} );
+	
+} );
